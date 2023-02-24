a9c19608e65eb847bb43e03d1410ff3e986ada55
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2023-02-25 01:04:24 +0300

    initial json dump fix

--- lab_1/server.py
+++ lab_1/server.py
@@ -7,7 +7,7 @@ from functions import readJson
 config = readJson()
 messages = queue.Queue()
 clients = []
-users = []
+users = set()
 mess = []
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,32 +22,35 @@ def recievePackage() -> None:
 
 def broadcastFunction() -> None:
     id = 0
+    
     while True:
         while not messages.empty():
             msg, adr = messages.get()
-            print(msg.decode())
+            message = msg.decode()
+            print(message)
             if adr not in clients:
                 clients.append(adr)
             for client in clients:
                 try:
-                    if msg.decode().startswith("SIGNUP_TAG:"):
-                        nickname = msg.decode()[msg.decode().index(":")+1:]
-                        users.append(nickname)
+                    if message.startswith("SIGNUP_TAG:"):
+                        nickname = message[message.index(":")+1:]
                         server.sendto(f"{nickname} online!".encode(), client)
+                        users.add(nickname)
                     else:
                         server.sendto(msg, client)
                 except:
                     clients.remove(client)
-            if msg.decode().startswith("SIGNUP_TAG:"):
-                mess.append({"id" : id, "sender" : nickname, "text" : f"{nickname} online!"})
-            else:
-                mess.append({"id" : id, "sender" : nickname, "text" : msg.decode()})
-            id += 1
-            print(mess)
-        with open('messages.json', 'w') as f:
-            json.dump({"chat_id" : 1, "users" : users, "messages" : mess}, f)
-
-
+                if message.startswith("SIGNUP_TAG:"):
+                    nickname = message[message.index(":")+1:]
+                    mess.append({"id" : id, "sender" : nickname, "text" : f"{nickname} online!"})
+                else:
+                    nickname = message[0:message.index(":")]
+                    mess.append({"id" : id, "sender" : nickname, "text" : message})
+                id += 1
+                print(mess)
+                print(users)
+            with open('messages.json', 'w+') as f:
+                json.dump({"chat_id" : 1, "users" : list(users), "messages" : mess}, f)
 
 thread_1 = threading.Thread(target=recievePackage)
 thread_2 = threading.Thread(target=broadcastFunction)
